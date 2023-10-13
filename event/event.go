ce35829e875ab45db35c6c8ad179542b28f4b500
gbatanov <gbatanov@yandex.ru>
2023-10-13 11:52:03 +0300

    v0.0.2 Работающая модель событий

--- event/event.go
+++ event/event.go
@@ -1,7 +1,75 @@
 package event
 
-import "log"
+import (
+	"log"
+	"time"
+)
+
+type Event struct {
+	Id      string
+	Message string
+}
+
+type EventEmitter struct {
+	Events []*Event
+}
 
 func init() {
 	log.Println("event init")
+}
+
+func EventEmitterCreate() *EventEmitter {
+	evEmitter := EventEmitter{}
+	evEmitter.Events = make([]*Event, 0)
+	return &evEmitter
+}
+
+// Create empty event with Id = id
+func (em *EventEmitter) CreateEvent(id string) *Event {
+	event := Event{Id: id, Message: ""}
+	em.Events = append(em.Events, &event)
+	return &event
+}
+
+// Find event in emitter, if not finded< create empty
+func (em *EventEmitter) GetEvent(id string) *Event {
+	for _, ev := range em.Events {
+		if ev.Id == id {
+			return ev
+		}
+	}
+	return em.CreateEvent(id)
+}
+
+// When a message arrives from the modem, id - command, msg - message
+func (em *EventEmitter) SetEvent(id string, msg string) {
+	event := em.GetEvent(id)
+	event.Message = msg
+}
+
+// Call before waiting
+func (em *EventEmitter) ResetEvent(id string) {
+	event := em.GetEvent(id)
+	event.Message = ""
+}
+
+// wait for a message with a given identifier
+func (em *EventEmitter) WaitEvent(id string, timeout time.Duration) string {
+	event := em.GetEvent(id)
+	if event.Message != "" {
+		return event.Message
+	}
+	timer1 := time.NewTimer(timeout)
+	for {
+		select {
+		case <-timer1.C:
+			timer1.Stop()
+			return ""
+		default:
+			if event.Message != "" {
+				return event.Message
+			}
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "log"
+import (
+	"gsm/event"
+	"log"
+	"sync"
+	"time"
+)
+
+const VERSION = "0.0.2"
 
 func main() {
 	log.Println("Start")
+	em := event.EventEmitterCreate()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		em.ResetEvent("AT")
+		result := em.WaitEvent("AT", time.Second*10)
+		log.Println(result)
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		em.ResetEvent("AT+")
+		result := em.WaitEvent("AT+", time.Second*3)
+		log.Println(result)
+		wg.Done()
+	}()
+	time.Sleep(time.Second * 1)
+	em.SetEvent("AT+", "OK+")
+	time.Sleep(time.Second * 5)
+	em.SetEvent("AT", "OK")
+	wg.Wait()
+	log.Println("End")
 }
